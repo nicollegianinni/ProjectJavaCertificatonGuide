0ad018bba30e5729c80c606c6198b6bb9325cb99
NICHOLLE GIANINNI <gianinni.nicolle@gmail.com>
2025-01-15 00:38:58 -0300

    step 1 loop while

---
+++ projeto modulo 1/src/LoopWhile.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class LoopWhile {
+    public static void main(String[] args) {
+
+        Scanner leitura = new Scanner(System.in);
+        double mediaAvaliação = 0;
+        double nota = 0;
+        int totalDeNotas = 0;
+
+        while (nota != -1) {
+            System.out.println("que nota você da para o filme?");
+            nota = leitura.nextDouble();
+
+            if (nota != -1) {
+                mediaAvaliação += nota;
+                totalDeNotas++;
+            }
+            System.out.println("Media de avaliação é " + mediaAvaliação / totalDeNotas);
+
+        }
+    }
+}
--- projeto modulo 1/src/loopFor.java
+++ projeto modulo 1/src/loopFor.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class loop {
+public class loopFor {
     public static void main(String[] args) {
 
         Scanner leitura = new Scanner(System.in);
